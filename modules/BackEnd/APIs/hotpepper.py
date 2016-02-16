117e75299b449722ef1fa23b598c2a6a2e149fff
hirokinakayama <h-nakayama@uec.ac.jp>
2016-02-16 22:57:59 +0900

    implement hotpepper api method

--- modules/BackEnd/APIs/hotpepper.py
+++ modules/BackEnd/APIs/hotpepper.py
@@ -10,28 +10,35 @@ class HotPepperGourmetAPI(object):
     def __init__(self, api_key=None):
         self.__api_key = os.environ.get('HOTPEPPER_API_KEY', api_key)
 
-    def search_restaurant(self, **kwargs):
-        # 指定するリクエストパラメータ
-        # key, small_area, food, budget
-        params = {'key': self.__api_key}
+    def __search(self, api_type, **kwargs):
+        params = {'key': self.__api_key, 'format': 'json'}
         params.update(kwargs)
-        url = self.BASE_URL.format('gourmet')
-        try:
-            response = requests.get(url, params).json()
-        except Exception:
-            print('Error has occured.')
-            raise
+        url = self.BASE_URL.format(api_type)
+        response = requests.get(url, params).json()
 
         return response
 
-    def search_area_name(self):
-        pass
+    def search_restaurant(self, **kwargs):
+        # 指定するリクエストパラメータ
+        # key, small_area, food, budget
+        response = self.__search('gourmet', **kwargs)
 
-    def search_food_name(self):
-        pass
+    def area_name2area_code(self, **kwargs):
+        # keyword 完全一致を優先させる
+        response = self.__search('small_area', **kwargs)
+        small_area_code = response['small_area']['code']
 
-    def search_budget(self):
-        pass
+        return small_area_code
+
+    def food_name2food_code(self, **kwargs):
+        # keyword 完全一致を優先させる
+        response = self.__search('food', **kwargs)
+        food_code = response['food']['code']
+
+        return food_code
+
+    def search_budget(self, **kwargs):
+        response = self.__search('budget', **kwargs)
 
     # エリア名、料理名、予算額を与えると、それぞれ対応するコードに変換するメソッドが必要？
     # エリア名に関してはsmall_areaAPIに名前を与えるとコードを返してくれる
