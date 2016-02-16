722ca7e966a6d2ab5e39953252b4ccb851ebaaff
Hironsan <light.tree.1.13@gmail.com>
2016-02-16 17:09:59 +0900

    implement search_restaurant

--- modules/BackEnd/APIs/hotpepper.py
+++ modules/BackEnd/APIs/hotpepper.py
@@ -1,7 +1,44 @@
 # -*- coding: utf-8 -*-
+import os
+import requests
 
 
 class HotPepperGourmetAPI(object):
 
-    def __init__(self):
+    BASE_URL = 'http://webservice.recruit.co.jp/hotpepper/{0}/v1/'
+
+    def __init__(self, api_key=None):
+        self.__api_key = os.environ.get('HOTPEPPER_API_KEY', api_key)
+
+    def search_restaurant(self, **kwargs):
+        # 指定するリクエストパラメータ
+        # key, small_area, food, budget
+        params = {'key': self.__api_key}
+        params.update(kwargs)
+        url = self.BASE_URL.format('gourmet')
+        try:
+            response = requests.get(url, params).json()
+        except Exception:
+            print('Error has occured.')
+            raise
+
+        return response
+
+    def search_area_name(self):
         pass
+
+    def search_food_name(self):
+        pass
+
+    def search_budget(self):
+        pass
+
+    # エリア名、料理名、予算額を与えると、それぞれ対応するコードに変換するメソッドが必要？
+    # エリア名に関してはsmall_areaAPIに名前を与えるとコードを返してくれる
+    # 料理名も同様
+    # 予算に関してはなさそうなので、対応させるロジックを組む必要がある
+
+
+if __name__ == '__main__':
+    api = HotPepperGourmetAPI()
+    api.search_restaurant()
