f84630d7b76f36d5fa63450cf755ef28832c5b55
h-nakayama <light.tree.1.13@gmail.com>
2016-08-01 06:26:52 +0900

    show google map

--- modules/LanguageGeneration/generator.py
+++ modules/LanguageGeneration/generator.py
@@ -8,7 +8,7 @@ class LanguageGenerator(object):
         pass
 
     def generate_sentence(self, dialogue_act):
-        sent = 'S: '
+        sent = ''
         if 'LOCATION' in dialogue_act:
             sent += '場所は{0}ですね。'.format(dialogue_act['LOCATION'])
         if 'GENRE' in dialogue_act:
@@ -27,7 +27,10 @@ class LanguageGenerator(object):
             restaurant = dialogue_act['restaurant']
             if restaurant:
                 name, address, access = restaurant['name'], restaurant['address'], restaurant['access']
-                sent += 'では、{0}がおすすめです。\n場所は{1}で{2}です。'.format(name, address, access)
+                lat, lng = restaurant['lat'], restaurant['lng']
+                sent += 'では、{0}がおすすめです。\n場所は{1}で{2}です。\n'.format(name, address, access)
+                url = 'https://maps.googleapis.com/maps/api/staticmap?center={0},{1}&size=640x480&zoom=14&markers={0},{1}'
+                sent += url.format(lat, lng)
             else:
                 sent += '申し訳ありません。条件に一致するお店が見つかりませんでした。'
         else:
