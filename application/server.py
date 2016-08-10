8797ba2b74ced70bab86d968f5f17e402d0cdc0a
Hironsan <light.tree.1.13@gmail.com>
2016-08-10 15:56:32 +0900

    implement websocket server

---
+++ application/server.py
@@ -0,0 +1,33 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+from application.bot import Bot
+
+
+class MessageServer(tornado.websocket.WebSocketHandler):
+    bots = {}
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print("on open")
+        self.bots[self] = Bot()
+        self.write_message('料理のジャンルや場所をおっしゃってください。')
+
+    def on_message(self, message):
+        print("on message")
+        print(message)
+        bot = self.bots[self]
+        self.write_message(bot.reply(message))
+
+    def on_close(self):
+        del self.bots[self]
+
+application = tornado.web.Application([(r'/ws', MessageServer)])
+
+
+if __name__ =="__main__":
+    application.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
